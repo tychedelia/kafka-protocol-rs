28cbc8bc82d5c2e81246d04fbc0c779874e185d5
Lucas Kent <rubickent@gmail.com>
2023-02-28 06:01:47 +1100

    minor typo+warning fixes (#18)

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -25,7 +25,7 @@ pub struct DecodeError;
 
 impl std::fmt::Display for DecodeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "DecodeErrror")
+        write!(f, "DecodeError")
     }
 }
 
